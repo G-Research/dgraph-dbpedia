e3caad1977bb5e7d2bc2058e976fd65a15dd297a
Enrico Minack <github@enrico.minack.dev>
2020-10-06 15:22:26 +0200

    Download skos categories dataset

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 RELEASE=2016-10
 DATASET=core-i18n
 LANGS=${2:-af als am an ar arz ast az azb ba bar be bg bn bpy br bs bug ca ce ceb ckb cs cv cy da de el en eo es et eu fa fi fo fr fy ga gd gl gu he hi hr hsb ht hu hy ia id io is it ja jv ka kk kn ko ku ky la lb li lmo lt lv mg min mk ml mn mr mrj ms my mzn nah nap nds ne new nl nn no oc or os pa pl pms pnb pt qu ro ru sa sah scn sco sh si sk sl sq sr su sv sw ta te tg th tl tr tt uk ur uz vec vi vo wa war yi yo zh}
-FILENAMES="labels infobox_properties interlanguage_links article_categories"
+FILENAMES="article_categories skos_categories labels infobox_properties interlanguage_links"
 EXT=.ttl.bz2
 
 base=${1:-$(pwd)/dbpedia}

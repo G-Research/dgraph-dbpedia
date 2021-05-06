8de023fae1879c2a13e5dcdaf2848cc2152083b4
Enrico Minack <github@enrico.minack.dev>
2021-05-06 09:08:12 +0200

    Move to v21.03.0, remove tmp symlink, fix schema pwd

--- dgraph.bulk.sh
+++ dgraph.bulk.sh
@@ -3,15 +3,14 @@
 if [[ $# -lt 3 ]]
 then
 	echo "Provide path to the data, where to bulk load the files,"
-	echo "and the files to load relative to the given data path"
+	echo "the schema and rdf files in data as mounted to /data."
 	exit 1
 fi
 
 data=$(cd "$1"; pwd)
 bulk=$(cd "$2"; pwd)
-schema=$(cd "$3"; pwd)
+schema="$3"
 
-mkdir -p $bulk
 cp dgraph.bulk.load.sh $bulk/
-docker run --rm -it -v "$data:/data" -v "$bulk:/dgraph" dgraph/dgraph:v20.11.0-rc1-142-g79f6e8edc /dgraph/dgraph.bulk.load.sh "$schema" "${@:4}"
+docker run --rm -it -v "$data:/data" -v "$bulk:/dgraph" dgraph/dgraph:v21.03.0 /dgraph/dgraph.bulk.load.sh "$schema" "${@:4}"
 

affec1bbc5c1626acbea853fa48706057cca1b86
Enrico Minack <github@enrico.minack.dev>
2021-12-03 17:36:07 +0100

    Use latest dgraph release

--- dgraph.bulk.sh
+++ dgraph.bulk.sh
@@ -13,5 +13,5 @@ bulk=$(cd "$2"; pwd)
 schema="$3"
 
 cp dgraph.bulk.load.sh $bulk/
-docker run --rm -it -v "$data:/data" -v "$bulk:/dgraph" dgraph/dgraph:v20.11.0-rc1-142-g79f6e8edc /dgraph/dgraph.bulk.load.sh "$schema" "${@:4}"
+docker run --rm -it -v "$data:/data" -v "$bulk:/dgraph" dgraph/dgraph:latest /dgraph/dgraph.bulk.load.sh "$schema" "${@:4}"
 

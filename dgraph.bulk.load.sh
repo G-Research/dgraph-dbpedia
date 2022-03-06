598377189deabe80114894b4be5afa050c1790a6
Enrico Minack <github@enrico.minack.dev>
2022-03-06 15:57:31 +0100

    Bulk load with future hostname of first zero

--- dgraph.bulk.load.sh
+++ dgraph.bulk.load.sh
@@ -25,14 +25,17 @@ echo
 
 function join { local IFS="$1"; shift; echo "$*"; }
 
+echo "127.0.0.1 dgraph-zero-0.dgraph-zero.default.svc.cluster.local" >> /etc/hosts
+ping -c 1 dgraph-zero-0.dgraph-zero.default.svc.cluster.local
+
 # start zero
 echo "starting zero"
 rm -rf /dgraph/zw
-dgraph zero >> /data/zero.log 2>&1 < /dev/null &
+dgraph zero --my dgraph-zero-0.dgraph-zero.default.svc.cluster.local:5080 >> /dgraph/zero.log 2>&1 < /dev/null &
 sleep 5
 
 # start bulk loader
 echo "bulk loading"
 rm -rf /dgraph/out /dgraph/xidmap
-dgraph bulk --store_xids --xidmap /dgraph/xidmap -j 4 --ignore_errors --tmp /dgraph/tmp -f "$(join , $(ls $data_dir/*))" -s "$schema_file" --format=rdf --out=/dgraph/out --replace_out 2>&1 | tee /dgraph/bulk.log
+dgraph bulk --zero dgraph-zero-0.dgraph-zero.default.svc.cluster.local:5080 --store_xids --xidmap /dgraph/xidmap -j 4 --ignore_errors --tmp /dgraph/tmp -f "$(join , $(ls $data_dir/*))" -s "$schema_file" --format=rdf --out=/dgraph/out --replace_out 2>&1 | tee /dgraph/bulk.log
 
